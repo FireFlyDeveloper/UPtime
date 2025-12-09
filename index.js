a16c7c584b51264b1a3d0cee229efe18f3b95968
FireFlyDeveloper <saludeskimdev@gmail.com>
2025-12-09 15:28:18 +0800

    install packages

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require("dotenv").config();
+const express = require('express');
+const cors = require("cors");
 
-app.get('/hello-world', (req, res) => {
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.get('/hello-world', (_req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
