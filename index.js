35125e9527e5d10ccbcda94a3dd91048b7980259
FireFlyDeveloper <saludeskimdev@gmail.com>
2025-12-09 19:27:57 +0800

    add post

--- index.js
+++ index.js
@@ -13,6 +13,20 @@ app.get('/hello-world', (_req, res) => {
   res.send('Hello World!')
 });
 
+app.post('/health', (req, res) => {
+    const { isOnline, passsword } = req.body;
+
+    if (passsword !== process.env.PASSWORD) {
+        return res.status(401).send('Unauthorized');
+    }
+
+    if (isOnline) {
+        console.log("Server is marked as ONLINE");
+    } else {
+        console.log("Server is marked as OFFLINE");
+    }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
